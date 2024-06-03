618512e9e93c95d628482d0a7b702ba65a80cd81
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-06-03 23:10:01 +0100

    mod: ticket class

--- contracts/script/DeployTicketFactory.s.sol
+++ contracts/script/DeployTicketFactory.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import {TicketFactory, Ticket} from "../src/TicketFactory.sol";
+import {TicketFactory} from "../src/TicketFactory.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
@@ -16,12 +16,6 @@ contract DeployFactoryScript is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         TicketFactory ticketFactory = new TicketFactory(owner, address(0));
-        uint256[] memory ticketFlavourPrices = new uint256[](3);
-        ticketFlavourPrices[0] = 1000000000000000000;
-        ticketFlavourPrices[1] = 2000000000000000000;
-        ticketFlavourPrices[2] = 3000000000000000000;
-
-        Ticket ticket = new Ticket(address(1), "name", "symbol", address(0), address(0), 50, 25, ticketFlavourPrices);
 
         vm.stopBroadcast();
     }
